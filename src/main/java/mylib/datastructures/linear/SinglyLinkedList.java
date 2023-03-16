5faf6559b2e8e576d6505dc572f5d5994234694e
AarshShah9 <aarshshah99@gmail.com>
2023-03-16 16:13:26 -0600

    added implementations for singleLL

--- src/main/java/mylib/datastructures/linear/SinglyLinkedList.java
+++ src/main/java/mylib/datastructures/linear/SinglyLinkedList.java
@@ -4,6 +4,23 @@ import main.java.mylib.datastructures.nodes.SingleNode;
 
 public class SinglyLinkedList {
 
-    SingleNode head;
+    private SingleNode head;
 
+    public SinglyLinkedList() {
+        head = null;
+    }
+
+    public void setHead(SingleNode head) {
+        this.head = head;
+    }
+
+    public SingleNode getHead() {
+        return head;
+    }
+
+    public void insertAtHead(int data) {
+        SingleNode newNode = new SingleNode(data);
+        newNode.setNext(head);
+        head = newNode;
+    }
 }
