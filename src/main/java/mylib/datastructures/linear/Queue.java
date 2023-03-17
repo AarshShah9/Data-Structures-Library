5477db2fc7e746b01f9640cf98f65a45f6ce418f
Fraserwill03 <williamgfraser2015@gmail.com>
2023-03-16 21:51:38 -0600

    finished linked list implementation of Queue

--- src/main/java/mylib/datastructures/linear/Queue.java
+++ src/main/java/mylib/datastructures/linear/Queue.java
@@ -17,4 +17,55 @@ public class Queue {
         return size == 0;
     }
 
+    public void enqueue(int data) {
+        SingleNode newNode = new SingleNode(data);
+        if (empty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public int dequeue() {
+        if (empty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        int data = head.getValue();
+        head = head.getNext();
+        size--;
+        return data;
+    }
+
+    public int peek() {
+        if (empty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return head.getValue();
+    }
+
+    public int search(int data) {
+        SingleNode current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.getValue() == data) {
+                return index;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return -1;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
 }
