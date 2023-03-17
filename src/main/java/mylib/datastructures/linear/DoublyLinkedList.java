980f661629fe701b6bcbafebc9449db235f364d5
AarshShah9 <aarshshah99@gmail.com>
2023-03-16 23:14:49 -0600

    finished main functionality of D and S LL

--- src/main/java/mylib/datastructures/linear/DoublyLinkedList.java
+++ src/main/java/mylib/datastructures/linear/DoublyLinkedList.java
@@ -5,18 +5,24 @@ import main.java.mylib.datastructures.nodes.DoubleNode;
 public class DoublyLinkedList<T> {
     private DoubleNode<T> head;
     private DoubleNode<T> tail;
+    private int size;
 
     public DoublyLinkedList() {
         head = null;
         tail = null;
+        size = 0;
     }
 
     public DoubleNode<T> getHead() {
-        return head;
+        return this.head;
     }
 
     public DoubleNode<T> getTail() {
-        return tail;
+        return this.tail;
+    }
+
+    public int getSize() {
+        return this.size;
     }
 
     public void insertAtHead(T data) {
@@ -29,6 +35,50 @@ public class DoublyLinkedList<T> {
             head.setPrevious(newNode);
             head = newNode;
         }
+        size++;
+    }
+
+    public void insertAtTail(T data) {
+        DoubleNode<T> newNode = new DoubleNode<T>(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void delete(T value) {
+        DoubleNode<T> current = head;
+        while (current != null) {
+            if (current.getValue().equals(value)) {
+                if (current == head) {
+                    head = head.getNext();
+                    if (head != null) {
+                        head.setPrevious(null);
+                    }
+                } else if (current == tail) {
+                    tail = tail.getPrevious();
+                    if (tail != null) {
+                        tail.setNext(null);
+                    }
+                } else {
+                    current.getPrevious().setNext(current.getNext());
+                    current.getNext().setPrevious(current.getPrevious());
+                }
+                break;
+            }
+            current = current.getNext();
+        }
+        size--;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
     }
 
 }
--- src/main/java/mylib/datastructures/linear/SinglyLinkedList.java
+++ src/main/java/mylib/datastructures/linear/SinglyLinkedList.java
@@ -5,19 +5,26 @@ import main.java.mylib.datastructures.nodes.SingleNode;
 public class SinglyLinkedList<T> {
 
     private SingleNode<T> head;
+    private int size;
 
     public SinglyLinkedList() {
         head = null;
+        size = 0;
     }
 
     public SingleNode<T> getHead() {
-        return head;
+        return this.head;
+    }
+
+    public int getSize() {
+        return this.size;
     }
 
     public void insertAtHead(T data) {
         SingleNode<T> newNode = new SingleNode<T>(data);
         newNode.setNext(head);
         head = newNode;
+        size++;
     }
 
     public void insertAtTail(T data) {
@@ -27,23 +34,27 @@ public class SinglyLinkedList<T> {
             current = current.getNext();
         }
         current.setNext(newNode);
+        size++;
     }
 
     public void delete(T value) {
         SingleNode<T> current = head;
-        SingleNode<T> previous = null;
         while (current != null) {
-            if (current.getValue() == value) {
-                if (previous == null) {
-                    head = current.getNext();
+            if (current.getValue().equals(value)) {
+                if (current == head) {
+                    head = head.getNext();
                 } else {
+                    SingleNode<T> previous = head;
+                    while (previous.getNext() != current) {
+                        previous = previous.getNext();
+                    }
                     previous.setNext(current.getNext());
                 }
-                return;
+                break;
             }
-            previous = current;
             current = current.getNext();
         }
+        size--;
     }
 
     public void clear() {
