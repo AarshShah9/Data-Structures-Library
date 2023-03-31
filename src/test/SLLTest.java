4d95279de238983625dadd80c9781c6b6137b495
AarshShah9 <aarshshah99@gmail.com>
2023-03-31 10:16:15 -0600

    Started adding tests

---
+++ src/test/SLLTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import main.java.mylib.datastructures.nodes.SNode;
+import main.java.mylib.datastructures.linear.SLL;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class SLLTest {
+
+}
---
+++ src/test/SNodeTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import main.java.mylib.datastructures.nodes.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+public class SNodeTest {
+
+    @Test
+    public void testConstructor() {
+        SNode<Integer> node = new SNode<>(1);
+
+        assertEquals(1, node.getValue());
+        assertNull(node.getNext());
+    }
+
+    @Test
+    public void testGetValue() {
+        SNode<Integer> node = new SNode<>(1);
+
+        assertEquals("Get value doesn't work", 1, node.getValue());
+    }
+
+    @Test
+    public void testSetValue() {
+        SNode<Integer> node = new SNode<>(1);
+
+        node.setValue(2);
+        assertEquals("Set value doesn't woek", 2, node.getValue());
+    }
+
+    @Test
+    public void testGetNext() {
+        SNode<Integer> node = new SNode<>(1);
+        SNode<Integer> node2 = new SNode<>(2);
+
+        node.setNext(node2);
+        assertEquals("Get next doesn't work", node2, node.getNext());
+    }
+
+    @Test
+    public void testSetNext() {
+        SNode<Integer> node = new SNode<>(1);
+        SNode<Integer> node2 = new SNode<>(2);
+
+        node.setNext(node2);
+        assertEquals("Set next doesn't work", node2, node.getNext());
+    }
+}
