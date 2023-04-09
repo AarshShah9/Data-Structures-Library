9101f3375762e926e99c027dbf5bb9e03fa360c1
AarshShah9 <aarshshah99@gmail.com>
2023-04-09 11:19:30 -0600

    added pom.xml file

---
+++ src/main/java/mylib/datastructures/App.java
@@ -0,0 +1,7 @@
+package main.java.mylib.datastructures;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Aarsh & Will's Data Structures Library!");
+    }
+}
